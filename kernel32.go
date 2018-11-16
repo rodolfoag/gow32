1c3c93be6af765f06828fa9754e3fe59dbc15db1
Kevin Golding <kevin@kgolding.co.uk>
2018-11-16 19:58:44 +0000

    added ReleaseMutex()

--- kernel32.go
+++ kernel32.go
@@ -8,7 +8,20 @@ import (
 var (
 	kernel32        = syscall.NewLazyDLL("kernel32.dll")
 	procCreateMutex = kernel32.NewProc("CreateMutexW")
+	procReleaseMutex = kernel32.NewProc("ReleaseMutex")
 )
+
+func ReleaseMutex(id uintptr) error {
+	_, _, err := procReleaseMutex.Call(
+		id,
+	)
+	switch int(err.(syscall.Errno)) {
+	case 0:
+		return nil
+	default:
+		return err
+	}
+}
 
 func CreateMutex(name string) (uintptr, error) {
 	ret, _, err := procCreateMutex.Call(
